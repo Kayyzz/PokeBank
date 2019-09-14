8a0d423339073780660ad50359da20cf016b4b75
Lizzie Kim <jihyunca@gmail.com>
2019-09-14 16:48:28 -0400

    Added form

--- App.js
+++ App.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import t from 'tcomb-form-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const Form  = t.form.Form;
+
+const User = t.struct({
+  user_name: t.String,
+  user_age: t.String,
+  user_budget: t.String,
+  terms: t.Boolean
+})
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  fields: {
+    user_name: {
+      error: 'You need to input a name!'
+    },
+    user_age: {
+      error: 'Let us know how old you are!'
+    },
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+  stylesheet: formStyles,
+};
+
+export default class App extends Component {
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c}
+          type={User} 
+          options={options}
+        />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
